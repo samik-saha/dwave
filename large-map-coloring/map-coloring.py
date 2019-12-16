aa65a0ab35aa3c60e0c12c71928f3104fbde8c99
Samik Saha <3612424+samik-saha@users.noreply.github.com>
2019-12-15 23:02:31 -0500

    map coloring

---
+++ large-map-coloring/map-coloring.py
@@ -0,0 +1,12 @@
+import networkx as nx
+import dwave_networkx as dnx
+from hybrid.reference.kerberos import KerberosSampler
+import matplotlib.pyplot as plt
+
+G = nx.read_adjlist('usa.adj', delimiter=',')
+coloring = dnx.min_vertex_coloring(G, sampler = KerberosSampler(), chromatic_ub=4, max_iter=10, convergence=3)
+node_colors = [coloring.get(node) for node in G.nodes()]
+if dnx.is_vertex_coloring(G, coloring):  # adjust the next line if using a different map
+  nx.draw(G, pos=nx.shell_layout(G, nlist = [list(G.nodes)[x:x+10] for x in range(0, 50, 10)] + [[list(G.nodes)[50]]]), with_labels=True, node_color=node_colors, node_size=400, cmap=plt.cm.rainbow)
+plt.show()
+
